654cc2496f2581842bbfa762037c15fc4cabb6fc
Bob Thiry <thygate@gmail.com>
2022-11-10 20:05:48 +0100

    update

--- depthmap.py
+++ depthmap.py
@@ -24,7 +24,7 @@ from repositories.midas.midas.transforms import Resize, NormalizeImage, PrepareF
 import numpy as np
 #import matplotlib.pyplot as plt
 
-scriptname = "DepthMap v0.1.7"
+scriptname = "DepthMap v0.1.8"
 
 class Script(scripts.Script):
 	def title(self):
@@ -37,16 +37,18 @@ class Script(scripts.Script):
 
 		compute_device = gr.Radio(label="Compute on", choices=['GPU','CPU'], value='GPU', type="index")
 		model_type = gr.Dropdown(label="Model", choices=['dpt_large','dpt_hybrid','midas_v21','midas_v21_small'], value='dpt_large', type="index", elem_id="model_type")
-		net_size = gr.Slider(minimum=256, maximum=1024, step=128, label='Net size', value=384)
+		net_width = gr.Slider(minimum=64, maximum=2048, step=64, label='Net width', value=384)
+		net_height = gr.Slider(minimum=64, maximum=2048, step=64, label='Net height', value=384)
+		match_size = gr.Checkbox(label="Match input size",value=False)
 		invert_depth = gr.Checkbox(label="Invert DepthMap (black=near, white=far)",value=False)
 		save_depth = gr.Checkbox(label="Save DepthMap",value=True)
 		show_depth = gr.Checkbox(label="Show DepthMap",value=True)
 		combine_output = gr.Checkbox(label="Combine into one image.",value=True)
 		combine_output_axis = gr.Radio(label="Combine axis", choices=['Vertical','Horizontal'], value='Horizontal', type="index")
 
-		return [compute_device, model_type, net_size, invert_depth, save_depth, show_depth, combine_output, combine_output_axis]
+		return [compute_device, model_type, net_width, net_height, match_size, invert_depth, save_depth, show_depth, combine_output, combine_output_axis]
 
-	def run(self, p, compute_device, model_type, net_size, invert_depth, save_depth, show_depth, combine_output, combine_output_axis):
+	def run(self, p, compute_device, model_type, net_width, net_height, match_size, invert_depth, save_depth, show_depth, combine_output, combine_output_axis):
 
 		def download_file(filename, url):
 			print("Downloading midas model weights to %s" % filename)
@@ -76,177 +78,178 @@ class Script(scripts.Script):
 
 		print("Loading midas model weights ..")
 
-		#"dpt_large"
-		if model_type == 0: 
-			model_path = f"{model_dir}/dpt_large-midas-2f21e586.pt"
-			print(model_path)
-			if not os.path.exists(model_path):
-				download_file(model_path,"https://github.com/intel-isl/DPT/releases/download/1_0/dpt_large-midas-2f21e586.pt")
-			model = DPTDepthModel(
-				path=model_path,
-				backbone="vitl16_384",
-				non_negative=True,
-			)
-			net_w, net_h = 384, 384
-			resize_mode = "minimal"
-			normalization = NormalizeImage(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+		try:
+			#"dpt_large"
+			if model_type == 0: 
+				model_path = f"{model_dir}/dpt_large-midas-2f21e586.pt"
+				print(model_path)
+				if not os.path.exists(model_path):
+					download_file(model_path,"https://github.com/intel-isl/DPT/releases/download/1_0/dpt_large-midas-2f21e586.pt")
+				model = DPTDepthModel(
+					path=model_path,
+					backbone="vitl16_384",
+					non_negative=True,
+				)
+				net_w, net_h = 384, 384
+				resize_mode = "minimal"
+				normalization = NormalizeImage(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
 
-		#"dpt_hybrid"
-		elif model_type == 1: 
-			model_path = f"{model_dir}/dpt_hybrid-midas-501f0c75.pt"
-			print(model_path)
-			if not os.path.exists(model_path):
-				download_file(model_path,"https://github.com/intel-isl/DPT/releases/download/1_0/dpt_hybrid-midas-501f0c75.pt")
-			model = DPTDepthModel(
-				path=model_path,
-				backbone="vitb_rn50_384",
-				non_negative=True,
-			)
-			net_w, net_h = 384, 384
-			resize_mode="minimal"
-			normalization = NormalizeImage(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
+			#"dpt_hybrid"
+			elif model_type == 1: 
+				model_path = f"{model_dir}/dpt_hybrid-midas-501f0c75.pt"
+				print(model_path)
+				if not os.path.exists(model_path):
+					download_file(model_path,"https://github.com/intel-isl/DPT/releases/download/1_0/dpt_hybrid-midas-501f0c75.pt")
+				model = DPTDepthModel(
+					path=model_path,
+					backbone="vitb_rn50_384",
+					non_negative=True,
+				)
+				net_w, net_h = 384, 384
+				resize_mode="minimal"
+				normalization = NormalizeImage(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
 
-		#"midas_v21"
-		elif model_type == 2: 
-			model_path = f"{model_dir}/midas_v21-f6b98070.pt"
-			print(model_path)
-			if not os.path.exists(model_path):
-				download_file(model_path,"https://github.com/AlexeyAB/MiDaS/releases/download/midas_dpt/midas_v21-f6b98070.pt")
-			model = MidasNet(model_path, non_negative=True)
-			net_w, net_h = 384, 384
-			resize_mode="upper_bound"
-			normalization = NormalizeImage(
-				mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-			)
-
-		#"midas_v21_small"
-		elif model_type == 3: 
-			model_path = f"{model_dir}/midas_v21_small-70d6b9c8.pt"
-			print(model_path)
-			if not os.path.exists(model_path):
-				download_file(model_path,"https://github.com/AlexeyAB/MiDaS/releases/download/midas_dpt/midas_v21_small-70d6b9c8.pt")
-			model = MidasNet_small(model_path, features=64, backbone="efficientnet_lite3", exportable=True, non_negative=True, blocks={'expand': True})
-			net_w, net_h = 256, 256
-			resize_mode="upper_bound"
-			normalization = NormalizeImage(
-				mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
-			)
-
-		# override network size
-		net_w, net_h = net_size, net_size
-
-		# init transform
-		transform = Compose(
-			[
-				Resize(
-					net_w,
-					net_h,
-					resize_target=None,
-					keep_aspect_ratio=True,
-					ensure_multiple_of=32,
-					resize_method=resize_mode,
-					image_interpolation_method=cv2.INTER_CUBIC,
-				),
-				normalization,
-				PrepareForNet(),
-			]
-		)
-
-		model.eval()
-		
-		# optimize
-		if device == torch.device("cuda"):
-			model = model.to(memory_format=torch.channels_last)  
-			if not cmd_opts.no_half:
-				model = model.half()
-
-		model.to(device)
-
-		print("Computing depthmap(s) ..")
-
-		# iterate over input (generated) images
-		for count in range(0,len(processed.images)):
-			# skip first (grid) image if count > 1
-			if count == 0 and len(processed.images) > 1:
-				continue
-
-			# input image
-			img = cv2.cvtColor(np.asarray(processed.images[count]), cv2.COLOR_BGR2RGB) / 255.0
-			img_input = transform({"image": img})["image"]
-
-			# compute
-			precision_scope = torch.autocast if shared.cmd_opts.precision == "autocast" and device == torch.device("cuda") else contextlib.nullcontext
-			with torch.no_grad(), precision_scope("cuda"):
-				sample = torch.from_numpy(img_input).to(device).unsqueeze(0)
-				if device == torch.device("cuda"):
-					sample = sample.to(memory_format=torch.channels_last) 
-					if not cmd_opts.no_half:
-						sample = sample.half()
-				prediction = model.forward(sample)
-				prediction = (
-					torch.nn.functional.interpolate(
-						prediction.unsqueeze(1),
-						size=img.shape[:2],
-						mode="bicubic",
-						align_corners=False,
-					)
-					.squeeze()
-					.cpu()
-					.numpy()
+			#"midas_v21"
+			elif model_type == 2: 
+				model_path = f"{model_dir}/midas_v21-f6b98070.pt"
+				print(model_path)
+				if not os.path.exists(model_path):
+					download_file(model_path,"https://github.com/AlexeyAB/MiDaS/releases/download/midas_dpt/midas_v21-f6b98070.pt")
+				model = MidasNet(model_path, non_negative=True)
+				net_w, net_h = 384, 384
+				resize_mode="upper_bound"
+				normalization = NormalizeImage(
+					mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
 				)
 
-			# output
-			depth = prediction
-			numbytes=2
-			depth_min = depth.min()
-			depth_max = depth.max()
-			max_val = (2**(8*numbytes))-1
+			#"midas_v21_small"
+			elif model_type == 3: 
+				model_path = f"{model_dir}/midas_v21_small-70d6b9c8.pt"
+				print(model_path)
+				if not os.path.exists(model_path):
+					download_file(model_path,"https://github.com/AlexeyAB/MiDaS/releases/download/midas_dpt/midas_v21_small-70d6b9c8.pt")
+				model = MidasNet_small(model_path, features=64, backbone="efficientnet_lite3", exportable=True, non_negative=True, blocks={'expand': True})
+				net_w, net_h = 256, 256
+				resize_mode="upper_bound"
+				normalization = NormalizeImage(
+					mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+				)
 
-			# check output before normalizing and mapping to 16 bit
-			if depth_max - depth_min > np.finfo("float").eps:
-				out = max_val * (depth - depth_min) / (depth_max - depth_min)
-			else:
-				out = np.zeros(depth.shape)
+			# override net size
+			if (match_size):
+				net_width, net_height = processed.width, processed.height
+
+			# init transform
+			transform = Compose(
+				[
+					Resize(
+						net_width,
+						net_height,
+						resize_target=None,
+						keep_aspect_ratio=True,
+						ensure_multiple_of=32,
+						resize_method=resize_mode,
+						image_interpolation_method=cv2.INTER_CUBIC,
+					),
+					normalization,
+					PrepareForNet(),
+				]
+			)
+
+			model.eval()
 			
-			# single channel, 16 bit image
-			img_output = out.astype("uint16")
+			# optimize
+			if device == torch.device("cuda"):
+				model = model.to(memory_format=torch.channels_last)  
+				if not cmd_opts.no_half:
+					model = model.half()
 
-			# invert depth map
-			if invert_depth:
-				img_output = cv2.bitwise_not(img_output)
+			model.to(device)
 
-			# three channel, 8 bits per channel image
-			img_output2 = np.zeros_like(processed.images[count])
-			img_output2[:,:,0] = img_output / 256.0
-			img_output2[:,:,1] = img_output / 256.0
-			img_output2[:,:,2] = img_output / 256.0
+			print("Computing depthmap(s) ..")
+			# iterate over input (generated) images
+			for count in range(0,len(processed.images)):
+				# skip first (grid) image if count > 1
+				if count == 0 and len(processed.images) > 1:
+					continue
 
-			# get generation parameters
-			if hasattr(p, 'all_prompts') and opts.enable_pnginfo:
-				info = create_infotext(p, p.all_prompts, p.all_seeds, p.all_subseeds, "", 0, 0)
-			else:
-				info = None
+				# input image
+				img = cv2.cvtColor(np.asarray(processed.images[count]), cv2.COLOR_BGR2RGB) / 255.0
+				img_input = transform({"image": img})["image"]
 
-			if not combine_output:
-				if show_depth:
-					processed.images.append(Image.fromarray(img_output))
-				if save_depth:
-					# only save 16 bit single channel image when PNG format is selected
-					if opts.samples_format == "png":
-						images.save_image(Image.fromarray(img_output), p.outpath_samples, "", processed.seed, p.prompt, opts.samples_format, info=info, p=p, suffix="_depth")
-					else:
-						images.save_image(Image.fromarray(img_output2), p.outpath_samples, "", processed.seed, p.prompt, opts.samples_format, info=info, p=p, suffix="_depth")
-			else:
-				img_concat = np.concatenate((processed.images[count], img_output2), axis=combine_output_axis)
-				if show_depth:
-					processed.images.append(Image.fromarray(img_concat))
-				if save_depth:
-					images.save_image(Image.fromarray(img_concat), p.outpath_samples, "", processed.seed, p.prompt, opts.samples_format, info=info, p=p, suffix="_depth")
+				# compute
+				precision_scope = torch.autocast if shared.cmd_opts.precision == "autocast" and device == torch.device("cuda") else contextlib.nullcontext
+				with torch.no_grad(), precision_scope("cuda"):
+					sample = torch.from_numpy(img_input).to(device).unsqueeze(0)
+					if device == torch.device("cuda"):
+						sample = sample.to(memory_format=torch.channels_last) 
+						if not cmd_opts.no_half:
+							sample = sample.half()
+					prediction = model.forward(sample)
+					prediction = (
+						torch.nn.functional.interpolate(
+							prediction.unsqueeze(1),
+							size=img.shape[:2],
+							mode="bicubic",
+							align_corners=False,
+						)
+						.squeeze()
+						.cpu()
+						.numpy()
+					)
 
-			#colormap = plt.get_cmap('inferno')
-			#heatmap = (colormap(img_output2[:,:,0] / 256.0) * 2**16).astype(np.uint16)[:,:,:3]
-			#processed.images.append(heatmap)
+				# output
+				depth = prediction
+				numbytes=2
+				depth_min = depth.min()
+				depth_max = depth.max()
+				max_val = (2**(8*numbytes))-1
 
+				# check output before normalizing and mapping to 16 bit
+				if depth_max - depth_min > np.finfo("float").eps:
+					out = max_val * (depth - depth_min) / (depth_max - depth_min)
+				else:
+					out = np.zeros(depth.shape)
+				
+				# single channel, 16 bit image
+				img_output = out.astype("uint16")
+
+				# invert depth map
+				if invert_depth:
+					img_output = cv2.bitwise_not(img_output)
+
+				# three channel, 8 bits per channel image
+				img_output2 = np.zeros_like(processed.images[count])
+				img_output2[:,:,0] = img_output / 256.0
+				img_output2[:,:,1] = img_output / 256.0
+				img_output2[:,:,2] = img_output / 256.0
+
+				# get generation parameters
+				if hasattr(p, 'all_prompts') and opts.enable_pnginfo:
+					info = create_infotext(p, p.all_prompts, p.all_seeds, p.all_subseeds, "", 0, 0)
+				else:
+					info = None
+
+				if not combine_output:
+					if show_depth:
+						processed.images.append(Image.fromarray(img_output))
+					if save_depth:
+						# only save 16 bit single channel image when PNG format is selected
+						if opts.samples_format == "png":
+							images.save_image(Image.fromarray(img_output), p.outpath_samples, "", processed.seed, p.prompt, opts.samples_format, info=info, p=p, suffix="_depth")
+						else:
+							images.save_image(Image.fromarray(img_output2), p.outpath_samples, "", processed.seed, p.prompt, opts.samples_format, info=info, p=p, suffix="_depth")
+				else:
+					img_concat = np.concatenate((processed.images[count], img_output2), axis=combine_output_axis)
+					if show_depth:
+						processed.images.append(Image.fromarray(img_concat))
+					if save_depth:
+						images.save_image(Image.fromarray(img_concat), p.outpath_samples, "", processed.seed, p.prompt, opts.samples_format, info=info, p=p, suffix="_depth")
+
+				#colormap = plt.get_cmap('inferno')
+				#heatmap = (colormap(img_output2[:,:,0] / 256.0) * 2**16).astype(np.uint16)[:,:,:3]
+				#processed.images.append(heatmap)
+		finally:
 			del model
 
 		return processed
